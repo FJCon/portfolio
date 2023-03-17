6130b856e7274e90ef22c9f0fa95d790c58cd99a
FJCon <conilfernando@gmail.com>
2023-03-17 14:10:19 -0300

    ejemplo

--- Script/main.js
+++ Script/main.js
@@ -1,1 +1,8 @@
 //main script
+let developer = {
+    name: "Fernado",
+    htmlAndCss: true,
+    javascript: true,
+    frontendTechs: ["html","css","javascript"],
+    backendTechs:["sql","java"]
+}
